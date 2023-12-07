6eb1ba552ddcc28fd5c9f11327a65294476ab2eb
malkoG <rijgndqw012@gmail.com>
2023-12-08 02:59:17 +0900

    Add support for mermaid plugin

--- gulpfile.js
+++ gulpfile.js
@@ -125,6 +125,7 @@ gulp.task('plugins', () => {
         { name: 'RevealNotes', input: './plugin/notes/plugin.js', output: './plugin/notes/notes' },
         { name: 'RevealZoom', input: './plugin/zoom/plugin.js', output: './plugin/zoom/zoom' },
         { name: 'RevealMath', input: './plugin/math/plugin.js', output: './plugin/math/math' },
+		{ name: 'RevealMermaid', input: './plugin/mermaid/plugin.js', output: './plugin/mermaid/mermaid' },
     ].map( plugin => {
         return rollup({
                 cache: cache[plugin.input],
